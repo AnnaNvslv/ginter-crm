290fe1272233ba2345ac0da94f1d25036a79a3f7
AnnaNvslv <ana.novosel.ns@gmail.com>
2026-07-12 17:57:38 +0200

    config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://flxewreibnkyfoccfjtg.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_GtVx_U91nCREZ-fq4yfLLw_NLsvQYGQ';
+
+const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
